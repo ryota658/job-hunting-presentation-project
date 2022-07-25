bfc23e029309dfe0dfb1eeaf3080f464e67aa399
ryota658 <gjzmeeds@i.softbank.jp>
2022-07-25 17:06:16 +0900

    add
    primarycolorとsecondarycolorの変更

--- src/resources/ts/theme/theme.ts
+++ src/resources/ts/theme/theme.ts
@@ -3,7 +3,14 @@ import { createTheme } from '@material-ui/core/styles';
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#009688',
+      main: '#64b5f6',
+      dark: "#9be7ff",
+      light: "#e3f2fd"
     },
+    secondary: {
+      main: "#f4511c",
+      light: "#ff844b",
+      dark: "#b91400"
+    }
   },
 });
